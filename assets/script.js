8d25616da2beb22286cce1e756b28d0d50d8a1f6
Emily Stewart <stewartemilym2014@gmail.com>
2021-02-25 19:37:49 -0700

    added button, and center align

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var  = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
